ae6ff6e59582b255334efbd6680b673533415a7a
YaniraAB <arenazas2.2@hotmail.com>
2018-01-16 21:00:41 -0500

    reconociendo espacios en blanco - app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
 $(document).ready(begin());
+function changingBlanks(tag) {
+  var arrayTitle = [];
+  var newText = [];
+  for (var i = 0; i < tag.length; i++) {
+    var num = tag.charCodeAt(i);
+    arrayTitle.push(num);
+  }
+  for (var i = 0; i < arrayTitle.length; i++) {
+    if (arrayTitle[i] === 32) {
+      newText.push('+');
+    } else {
+      var letters = String.fromCharCode(arrayTitle[i]);
+      newText.push(letters);
+    }
+  }
+  newText = newText.join('');
+  console.log(newText);
+}
 function begin() {
   $('#search').on('click', function() {
     var text = '';
     var tag = $('#input-search').val();
+    changingBlanks(tag);
     $.getJSON('http://www.omdbapi.com/?apikey=3a181f1c&t=' + tag, function(data) {
       $.each(data, function(i, Poster) {
         if (i === 'Poster') {

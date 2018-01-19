26fc9b804949913193abd440f19557bc4981f0eb
DaphneMilena <daphnemilena1013@gmail.com>
2018-01-19 09:49:13 -0500

    añadiendo readme, vista html

--- js/autentication.js
+++ js/autentication.js
@@ -9,100 +9,100 @@ var config = {
 };
 firebase.initializeApp(config);
 
-$(document).ready(function() {
-  var $valEmail = $('#email').val();
-  var $valPassword = $('#password').val();
+var $valEmail = $('#email').val();
+var $valPassword = $('#password').val();
+var $valEmail2 = $('#email2').val();
+var $valPassword2 = $('#password2').val();
+var photoUser = $('#photo-user');
+var nameUser = $('#name-user');
 
-  firebase.auth().createUserWithEmailAndPassword($valEmail, $valPassword).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode === 'auth/weak-password') {
-      alert('The password is too weak.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
-  });
-
-  firebase.auth().signInWithEmailAndPassword($valEmail, $valPassword).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode === 'auth/wrong-password') {
-      alert('Wrong password.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
-  });
-
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // ...
-    } else {
-      // User is signed out.
-      // ...
-    }
-  });
-  // Registro de usuarios (signUp)
-  $('#btn-signUp-js').click(function(event) {
-  // event.preventDefault();
-
-    firebase.auth().createUserWithEmailAndPassword($valEmail, $valPassword)
-      .then(function(result) {
-        alert('Autentificación correcta');
-      // window.location.href = '../../index.html';
-        // verifyAccount();
-      })
-      .catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-        alert('Ya está registrado');
-      });
-  });
-
-  // iniciar sesión de usuario
-  $('#btn-login-js').click(function() {
-    var $valEmail2 = $('#email2').val();
-    var $valPassword2 = $('#password2').val();
-    firebase.auth().signInWithEmailAndPassword($valEmail2, $valPassword2)
-      .then(function(result) {
-        alert('Autentificación correcta');
-      // window.location.href = '../views/home/index.html';
-      })
-      .catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert('Credenciales Incorrectas, Ingrese Nuevamente');
-      });
-  });
-
-  $('#signInButtonG').on('click', function() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  });
-
-  function saveDate(user) {
-    var usuario = {
-      uid: user.uid,
-      nombre: user.displayName,
-      email: user.email,
-      foto: user.photoURL
-    };
-    firebase.database().ref('users/' + user.uid).set(usuario);
-  }
+// Registro de usuarios (signUp)
+$('#btn-signUp-js').click(function(event) {
+  firebase.auth().createUserWithEmailAndPassword($valEmail, $valPassword)
+    .then(function(result) {
+      window.location.href = '../index.html';
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
 });
+
+// iniciar sesión de usuario
+$('#btn-login-js').click(function() {
+  var $valEmail2 = $('#email2').val();
+  var $valPassword2 = $('#password2').val();
+  firebase.auth().signInWithEmailAndPassword($valEmail2, $valPassword2)
+    .then(function(result) {
+      // alert('Autentificación correcta');
+      window.location.href = '../views/home.html';
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert('Credenciales Incorrectas, Ingrese Nuevamente');
+    });
+});
+
+  
+// cuenta de Google
+function IngresoGoogle() {
+  if (!firebase.auth().currentUser) {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+      window.location.href = '../views/home.html';
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      if (errorcode === 'auth/account-exists-with-different-credential') {
+        alert('Es el mismo usuario');
+      }
+    });
+  } else {
+    firebase.auth().signOut();
+  }
+};
+
+$('#btnGoogle').on('click', IngresoGoogle);
+
+
+// Extraer datos de usuario
+var photoUser = $('#photo-user');
+var nameUser = $('#name-user');
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) 
+    // El usuario ha iniciado sesión
+  var name = user.displayName;
+  var email = user.email;
+  var photoUrl = user.photoURL;
+  var emailVerified = user.emailVerified;
+  var uid = user.uid;
+  console.log(user);
+  nameUser.text(name);
+  photoUser.attr('src', photoUrl);
+});
+
+// Cerrar sesión
+$('#signOut').on('click', function() {
+  firebase.auth().signOut().then(function(user) {
+    console.log('saliendo...');
+    window.location.href = '../index.html';
+  }).catch(function(error) {
+    console.log(error);
+  });
+});
+
+// // Funcion para icono me encanta
+// $('.favorite').on('click', function() {
+//   $(this).toggleClass('like');
+// });
+
 
